2b6b5d78eee3739b8d3b34f803f0b8ab9307481b
Lukáš Franc <lf@antstudio.cz>
2023-12-28 19:04:26 +0100

    remove component imageDialog and some title line

--- components/personalCard.tsx
+++ components/personalCard.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import lflogo from "../public/images/lukas-franc-photography-white.svg";
 
 export const PersonalCard = () => {
+	// todo: add social media links
 	return (
 		<div
 			className="flex min-h-96 flex-col items-center justify-center gap-4 bg-white/5 px-6 pb-16 pt-16 text-center text-white">
 			<Image priority src={lflogo} alt="Lukas Franc Photography" width="300" />
 			<div className="flex gap-4">
-				soc ikony
+				<span className="social"></span>
 			</div>
 		</div>
 	);

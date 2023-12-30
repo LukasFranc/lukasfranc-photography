39d59f6234e0c4ddbd0486ba8da630494f73c1ff
Lukáš Franc <lf@antstudio.cz>
2023-12-30 10:28:57 +0100

    change text on personal card

--- components/personalCard.tsx
+++ components/personalCard.tsx
@@ -9,11 +9,12 @@ export const PersonalCard = () => {
 			className="flex min-h-96 flex-col items-center justify-center gap-5 bg-white/5 px-6 pb-16 pt-16 text-center text-white">
 			<Image priority src={lflogo} alt="Lukas Franc Photography" width="300" />
 			<div className="flex gap-4 flex-col italic text-sm">
-				<p>Vítám vás na svém fotografickém webu, kde se setkává vášeň pro krajinu s uměním zachycení krásy okolního světa. Každý snímek je pro mě nejen záběrem, ale i příběhem.</p>
-				<p>Snažím se zachytit to nejdůležitější - atmosféru, emoce a jedinečnost každého místa.</p>
+				<p>Vítám vás na svém fotografickém webu, kde se snažím prezentovat moji vášeň pro krajinu a přírodu obecně, s uměním její zachycení pomocí fotoaparátu.</p>
+				<p>Snažím se vždy zachytit to nejdůležitější - atmosféru, emoce a jedinečnost každého místa.</p>
 				<span className="social"></span>
 			</div>
 			<div className="flex gap-1 flex-col">
+				<p className="italic text-sm">V případě zájmu mě neváhejte kontaktovat.</p>
 				<Link href="https://www.lukasfranc.cz" className="no-underline hover:underline">www.lukasfranc.cz</Link>
 				<Link href="mailto:info@lukasfranc.cz" className="no-underline hover:underline">info@lukasfranc.cz</Link>
 			</div>
